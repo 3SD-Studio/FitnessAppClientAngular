e15afabe4ead0340cad2aaa73655c94a538af44b
Michał Pilch <m.pilch2002@gmail.com>
2023-04-30 17:30:55 +0200

    Test api connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'clientangular';
+  
+  // TEST IF CODE CAN CONNECT TO API. ONLY FOR TESTING PURPOSES
+  ngOnInit() : void {
+    fetch("https://localhost:7093/WeatherForecast").then((response) => response.json()).then((data) => console.log(data));
+  }
 }
