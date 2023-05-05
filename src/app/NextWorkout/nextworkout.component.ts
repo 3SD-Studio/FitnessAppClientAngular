336308f94f58ec89db5fe70b732a555f49fc34c2
Michał Pilch <mp300455@student.polsl.pl>
2023-05-05 23:44:32 +0200

    Next workout init

--- src/app/NextWorkout/nextworkout.component.ts
+++ src/app/NextWorkout/nextworkout.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./nextworkout.component.css']
 })
 export class NextworkoutComponent {
+  nextWorkoutDate : string;
+  chosenSet : string;
 
+  exerciseSetsList = [ 
+    'Legs',
+    'Arms',
+    'Chest',
+    'Back',
+    'Cardio'
+  ];
+
+  constructor() {
+    this.nextWorkoutDate = new Date().toLocaleDateString();
+    this.chosenSet = this.exerciseSetsList[getRandomInt(5)];
+  }
+}
+
+function getRandomInt(max : number) {
+  return Math.floor(Math.random() * max);
 }
