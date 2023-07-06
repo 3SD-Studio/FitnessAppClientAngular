7873a3e0a29b96afb2a40aad38404bea1c7c0ad0
Dave <dj300758@student.polsl.pl>
2023-07-06 13:08:03 +0200

    Added setter and getter.

--- src/app/Calendar/calendar.component.ts
+++ src/app/Calendar/calendar.component.ts
@@ -9,11 +9,28 @@ import { CalendarDate, CalendarService } from '../calendar.service';
 
 
 export class CalendarComponent {
-    calendarArray : CalendarDate[][];
-    month : string;
+    #calendarArray : CalendarDate[][];
+    #month : string;
 
-    constructor(service: CalendarService) { 
-        this.calendarArray = service.getCalendarArray();
-        this.month = service.getMonthString();
+    constructor(service: CalendarService) {
+        this.#calendarArray = service.getCalendarArray();
+        this.#month = service.getMonthString();
     }
+
+    get month(){
+      return this.#month;
+    }
+
+    get calendarArray(){
+      return this.#calendarArray;
+    }
+
+    set month(value: string){
+      this.#month = value;
+    }
+
+    set calendarArray(value: CalendarDate[][]){
+      this.#calendarArray = value;
+    }
+
 }
