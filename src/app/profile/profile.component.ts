2540ce9b2c6585c0617313186f9c1c91fe4af045
Dave <dj300758@student.polsl.pl>
2023-07-28 15:01:51 +0200

    Futher work on Profile Page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -7,7 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  name:String;
+  surname:String;
+  tel_num:String|Number;
+  sex:String;
+  address:String;
+  town:String;
+  country:String;
+  email:String;
+
+  constructor() {
+    this.name = "Dawid";
+    this.surname = "Jeziorski";
+    this.tel_num = 123456789;
+    this.email = "dawjez123@gmail.com"
+    this.sex = "male";
+    this.address = "Wrzosowa 15";
+    this.town= "Bazantowo";
+    this.country = "Poland";
+   }
 
   ngOnInit(): void {
   }
