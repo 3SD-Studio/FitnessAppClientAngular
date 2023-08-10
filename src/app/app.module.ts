9392886284b96bb1e2f6952dfa76f100e58e70d5
Dave <dj300758@student.polsl.pl>
2023-08-10 23:57:32 +0200

    New components, nested routing not working yet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,16 +10,22 @@ import { NextworkoutComponent } from './nextworkout/nextworkout.component';
 import { ProgressComponent } from './progress/progress.component';
 import { FooterComponent } from './footer/footer.component';
 import { CalendarItemComponent } from './calendar-item/calendar-item.component';
-import { CalendarService } from './calendar.service';
 import { HomeComponent } from './home/home.component';
-import { CreatorModule } from './creator/creator.module';
 import { MainComponent } from './creator/main/main.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ExerciseComponent } from './creator/exercise/exercise.component';
+import { WorkoutComponent } from './creator/workout/workout.component';
+
+
+import { CalendarService } from './calendar.service';
 
 
 const routes = [
   { path: '', component: HomeComponent },
-  { path: 'creator', component: MainComponent},
+  { path: 'creator', component: MainComponent, children:[
+    {path: 'exercise', component: ExerciseComponent},
+    {path: 'workout', component: WorkoutComponent}
+  ]},
   { path: 'profile', component: ProfileComponent}
 ]
 
@@ -34,11 +40,11 @@ const routes = [
     CalendarItemComponent,
     HomeComponent,
     ProfileComponent,
-
+    ExerciseComponent,
+    WorkoutComponent
   ],
   imports: [
     BrowserModule,
-    CreatorModule,
     RouterModule.forRoot(routes)
   ],
   providers: [CalendarService],
